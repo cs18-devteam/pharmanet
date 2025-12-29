31e0ef9a043d0dc5d7696d7af1a14f37d7438e94
Hamdha <mhcnhamdha2003@gmail.com>
2025-12-29 12:46:02 +0530

    media query for landing page. want to do changes.

--- server/common/ipaddress.js
+++ server/common/ipaddress.js
@@ -1,6 +1,6 @@
 const os = require('node:os');
-const ipaddress = os.networkInterfaces()["en0"]?.filter(obj => obj.family == "IPv4")[0].address;
-
+const ipaddress = os.networkInterfaces()['Wi-Fi']?.filter(obj => obj.family == "IPv4")[0].address;
+// console.log(os.networkInterfaces())
 
 module.exports = ipaddress;
 

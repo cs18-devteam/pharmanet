52dd9141771ddb8952fd84bb3d72e4443abf0702
ChamaniNiroshani <mpcniroshani@gmail.com>
2026-04-10 11:06:10 +0530

    transaction top bar updated

--- server/public/js/controller/pharmacy/pharmacy.dashboard.transactions.controller.js
+++ server/public/js/controller/pharmacy/pharmacy.dashboard.transactions.controller.js
@@ -4,7 +4,6 @@ import { getRequestData } from "../../model/pharmacy/fetchTransactionsData.js";
 import { updateTable } from "../../view/pharmacy/updateTransactionTable.js";
 import { getStaffSummary } from "../../model/pharmacy/fetchTransactionsData.js";
 
-
 export default async function init() {
   const table = document.querySelector(".transaction_table tbody");
   const startDate = document.getElementById("date_start");
@@ -15,12 +14,11 @@ export default async function init() {
   const cardOnline = document.querySelector(".stat-card .online span");
   const incomeValue = document.querySelector(".income__value");
   const summeryByStaff = document.querySelector(
-    ".summery_staff_wise.summery_block"
-);
-  const summeryOfPayment = document.querySelector(
-    ".summery_payment_method_wise.summery_block"
+    ".summery_staff_wise.summery_block",
   );
-  
+  const summeryOfPayment = document.querySelector(
+    ".summery_payment_method_wise.summery_block",
+  );
 
   //print option
 
@@ -28,270 +26,233 @@ export default async function init() {
 
   if (printBtn) {
     printBtn.addEventListener("click", () => {
-       console.log("PRINT CLICKED");
+      console.log("PRINT CLICKED");
       window.print();
     });
-  }else {
-  console.log("PRINT BUTTON NOT FOUND");
-}
-
-
-
+  } else {
+    console.log("PRINT BUTTON NOT FOUND");
+  }
 
   //console.log("init");
   const getRequestDatas = await getRequestData();
   //const getStaffDatas = await getStaffData();
-  updateTable(getRequestDatas)
-  filterTodayTransaction(getRequestDatas)
-  filterTransactionByDateRange(getRequestDatas)
-  totalAmountOfCachOnline(getRequestDatas)
-  totalAmountOfCachOffline(getRequestDatas)
-  totalAmountOfcardOnline(getRequestDatas)
-  totalAmountOfCardOffline(getRequestDatas)
-  updateTodayTransaction(getRequestDatas)
-  updateStaffWiseSummary()
+  updateTable(getRequestDatas);
+  filterTodayTransaction(getRequestDatas);
+  filterTransactionByDateRange(getRequestDatas);
+  totalAmountOfCachOnline(getRequestDatas);
+  totalAmountOfCachOffline(getRequestDatas);
+  totalAmountOfcardOnline(getRequestDatas);
+  totalAmountOfCardOffline(getRequestDatas);
+  updateTodayTransaction(getRequestDatas);
+  updateStaffWiseSummary();
   //printTransaction()
-  
 }
 
-
-export function filterTodayTransaction({results : transactionData}){
-
+export function filterTodayTransaction({ results: transactionData }) {
   const currentDate = new Date().toISOString().split("T")[0];
 
   //console.log(currentDate);
-  
+
   const todayTotal = transactionData
-    .filter(tr => {
+    .filter((tr) => {
       const trDate = new Date(tr.transactionDateTime)
         .toISOString()
         .split("T")[0];
 
-        return trDate === currentDate;
+      return trDate === currentDate;
     })
-    .reduce((sum , tr) => sum + Number(tr.amount),0);
+    .reduce((sum, tr) => sum + Number(tr.amount), 0);
 
+  console.log(todayTotal);
 
-    console.log(todayTotal);
-
-    return todayTotal;
-    
+  return todayTotal;
 }
 
-export function updateTodayTransaction({results : transactionData}){
-
+export function updateTodayTransaction({ results: transactionData }) {
   const incomeValue = document.querySelector(".income__value");
-  const totalAmount = filterTodayTransaction({results : transactionData});
+  const totalAmount = filterTodayTransaction({ results: transactionData });
 
   console.log(totalAmount);
-  incomeValue.innerHTML=totalAmount;
-  
+  incomeValue.innerHTML = totalAmount;
 }
 
-
-export function filterTransactionByDateRange({results : data}){
-
+export function filterTransactionByDateRange({ results: data }) {
   const table = document.querySelector(".transaction_table tbody");
   const startDate = document.getElementById("date_start");
   const endDate = document.getElementById("date_end");
 
-  if(!table || !startDate || !endDate)return
+  if (!table || !startDate || !endDate) return;
 
-  function eventHandle(e){
-    if(e.key !== "Enter")return
+  function eventHandle(e) {
+    if (e.key !== "Enter") return;
 
     e.preventDefault();
 
     const startValue = startDate.value;
     const endValue = endDate.value;
 
-    if(!startValue || !endValue){
-      console.warn("Select Both Date")
-      return
+    if (!startValue || !endValue) {
+      console.warn("Select Both Date");
+      return;
     }
 
-
     //filter data
-    const filteredDate = data.filter(tr => {
+    const filteredDate = data.filter((tr) => {
       const trDate = new Date(tr.transactionDateTime)
         .toISOString()
         .split("T")[0];
 
-        return trDate >= startValue && trDate <= endValue
-    })
+      return trDate >= startValue && trDate <= endValue;
+    });
 
     //map filter data
-    const mapFilterData = filteredDate.map(tr => {
+    const mapFilterData = filteredDate
+      .map((tr) => {
+        const dateObj = new Date(tr.transactionDateTime);
+        const date = dateObj.toISOString().split("T")[0];
+        const time = dateObj.toISOString().split("T")[1].slice(0, 5);
 
-      const dateObj = new Date(tr.transactionDateTime);
-      const date = dateObj.toISOString().split("T")[0];
-      const time = dateObj.toISOString().split("T")[1].slice(0,5);
-      
-      return`
+        return `
         <tr>
           <td>${tr.orderId}</td>
           <td>${tr.staffID || "-"}</td>
           <td>${date}</td>
           <td>${time}</td>
           <td class="cash">
-              ${(tr.method)}
+              ${tr.method}
           </td>
           <td class="online">
-              ${(tr.type)}
+              ${tr.type}
           </td>
           <td>${Number(tr.amount).toLocaleString()}</td>
         </tr>
-      `
-    }).join(' ');
+      `;
+      })
+      .join(" ");
 
     table.innerHTML = mapFilterData;
-      
-
-
   }
 
-  startDate.addEventListener("keydown",eventHandle);
+  startDate.addEventListener("keydown", eventHandle);
   endDate.addEventListener("keydown", eventHandle);
-
 }
 
-
-export function totalAmountOfCachOnline({results : data}){
-
-
+export function totalAmountOfCachOnline({ results: data }) {
   const cashOnline = document.querySelector(".stat-cash .online span");
   const prograss = document.querySelector(".bar-cash__online span");
-    //console.log(cashOffline);
-    const today = new Date().toISOString().split("T")[0];
+  //console.log(cashOffline);
+  const today = new Date().toISOString().split("T")[0];
 
-    const todayCashPayment = data
-      .filter(tr => {
-        const trDate= new Date(tr.transactionDateTime)
+  const todayCashPayment = data
+    .filter((tr) => {
+      const trDate = new Date(tr.transactionDateTime)
         .toISOString()
         .split("T")[0];
 
-        return trDate === today && tr.method === "Cash" && tr.type === "Online";
-      })
-      .reduce((sum , tr) => sum + Number(tr.amount),0);
-    
-    
-      //console.log("today cash payment");
-      //console.log(todayCashPayment);
-      cashOnline.innerHTML = todayCashPayment;
+      return trDate === today && tr.method === "cash" && tr.type === "online";
+    })
+    .reduce((sum, tr) => sum + Number(tr.amount), 0);
 
-      const todayTransaction = filterTodayTransaction({results : data});
-      
-      const percentage = todayTransaction > 0 ?
-       Math.floor((todayCashPayment / todayTransaction) * 100)
-       :0 ;
+  //console.log("today cash payment");
+  //console.log(todayCashPayment);
+  cashOnline.innerHTML = todayCashPayment;
 
-      prograss.style.width= `${percentage}%`;
-      prograss.innerHTML=`${percentage}%`;
-      //console.log(percentage);
+  const todayTransaction = filterTodayTransaction({ results: data });
 
-    
+  const percentage =
+    todayTransaction > 0
+      ? Math.floor((todayCashPayment / todayTransaction) * 100)
+      : 0;
+
+  prograss.style.width = `${percentage}%`;
+  prograss.innerHTML = `${percentage}%`;
+  //console.log(percentage);
 }
 
-export function totalAmountOfCachOffline({results : data}){
+export function totalAmountOfCachOffline({ results: data }) {
+  const cashOffline = document.querySelector(".stat-cash .offline span");
+  const prograss = document.querySelector(".bar-cash__offline span");
+  //console.log(cashOffline);
+  const today = new Date().toISOString().split("T")[0];
 
-    const cashOffline = document.querySelector(".stat-cash .offline span");
-    const prograss = document.querySelector(".bar-cash__offline span")
-    //console.log(cashOffline);
-    const today = new Date().toISOString().split("T")[0];
-
-    const todayCashPayment = data
-      .filter(tr => {
-        const trDate= new Date(tr.transactionDateTime)
+  const todayCashPayment = data
+    .filter((tr) => {
+      const trDate = new Date(tr.transactionDateTime)
         .toISOString()
         .split("T")[0];
 
-        return trDate === today && tr.method === "Cash" && tr.type === "Offline";
-      })
-      .reduce((sum , tr) => sum + Number(tr.amount),0);
-    
-    
-      //console.log("today cash payment");
-      //console.log(todayCashPayment);
-      cashOffline.innerHTML = todayCashPayment;
+      return trDate === today && tr.method === "cash" && tr.type === "offline";
+    })
+    .reduce((sum, tr) => sum + Number(tr.amount), 0);
 
-      const todayTransaction = filterTodayTransaction({results : data});
-      const percentage = todayTransaction > 0?  Math.floor((todayCashPayment/todayTransaction)*100)
-      :0 ;
+  //console.log("today cash payment");
+  //console.log(todayCashPayment);
+  cashOffline.innerHTML = todayCashPayment;
 
-      prograss.style.width= `${percentage}%`;
-      prograss.innerHTML= `${percentage}%`;
+  const todayTransaction = filterTodayTransaction({ results: data });
+  const percentage =
+    todayTransaction > 0
+      ? Math.floor((todayCashPayment / todayTransaction) * 100)
+      : 0;
 
-    
+  prograss.style.width = `${percentage}%`;
+  prograss.innerHTML = `${percentage}%`;
 }
 
-
-export function totalAmountOfcardOnline({results : datas}){
-
+export function totalAmountOfcardOnline({ results: datas }) {
   const cardOnline = document.querySelector(".stat-card .online span");
   const prograss = document.querySelector(".bar-card__online span");
 
   const today = new Date().toISOString().split("T")[0];
 
   const todayOnlineCardPayment = datas
-    .filter(tr => {
-      const date = new Date(tr.transactionDateTime)
-      .toISOString()
-      .split("T")[0];
+    .filter((tr) => {
+      const date = new Date(tr.transactionDateTime).toISOString().split("T")[0];
 
-      return date === today && tr.method === "Card" && tr.type === "Online";
+      return date === today && tr.method === "card" && tr.type === "online";
     })
-    .reduce((sum , tr) => sum + Number(tr.amount),0);
+    .reduce((sum, tr) => sum + Number(tr.amount), 0);
 
-    cardOnline.innerHTML = todayOnlineCardPayment;
+  cardOnline.innerHTML = todayOnlineCardPayment;
 
-    const todayTransaction = filterTodayTransaction({ results : datas});
-    const percetage = todayTransaction > 0?
-     Math.floor((todayOnlineCardPayment/todayTransaction)*100)
-     :0 ;
+  const todayTransaction = filterTodayTransaction({ results: datas });
+  const percetage =
+    todayTransaction > 0
+      ? Math.floor((todayOnlineCardPayment / todayTransaction) * 100)
+      : 0;
 
-    prograss.style.width = `${percetage}%`;
-    prograss.innerHTML= `${percetage}%`;
+  prograss.style.width = `${percetage}%`;
+  prograss.innerHTML = `${percetage}%`;
 }
 
-
 export function totalAmountOfCardOffline({ results: data }) {
-
   const cardOffline = document.querySelector(".stat-card .offline span");
   const prograss = document.querySelector(".bar-card__offline span");
   const today = new Date().toISOString().split("T")[0];
 
   const filteredDate = data
-    .filter(tr => {
-      const date = new Date(tr.transactionDateTime)
-        .toISOString()
-        .split("T")[0];
+    .filter((tr) => {
+      const date = new Date(tr.transactionDateTime).toISOString().split("T")[0];
 
-      return date === today && tr.method === "Card" && tr.type === "Offline";
+      return date === today && tr.method === "card" && tr.type === "offline";
     })
     .reduce((sum, tr) => sum + Number(tr.amount), 0);
 
   cardOffline.innerHTML = filteredDate;
   console.log(filteredDate);
 
-  const todayTransaction = filterTodayTransaction({ results : data});
-  const percetage = todayTransaction > 0 ? 
-   Math.floor((filteredDate/todayTransaction)*100)
-   :0;
+  const todayTransaction = filterTodayTransaction({ results: data });
+  const percetage =
+    todayTransaction > 0
+      ? Math.floor((filteredDate / todayTransaction) * 100)
+      : 0;
 
   prograss.style.width = `${percetage}%`;
-  prograss.innerHTML= `${percetage}%`;
-
+  prograss.innerHTML = `${percetage}%`;
 }
 
-
-
-
-
 export async function updateStaffWiseSummary() {
-
-  const container = document.querySelector(
-    ".summery_staff_wise.summery_block"
-  );
+  const container = document.querySelector(".summery_staff_wise.summery_block");
 
   if (!container) return;
 
@@ -302,7 +263,9 @@ export async function updateStaffWiseSummary() {
     return;
   }
 
-  const html = results.map(staff => `
+  const html = results
+    .map(
+      (staff) => `
     <div class="analytics">
       <div class="description">
         <div class="name">${staff.staffName}</div>
@@ -311,7 +274,9 @@ export async function updateStaffWiseSummary() {
       </div>
       <div class="bar"></div>
     </div>
-  `).join("");
+  `,
+    )
+    .join("");
 
   container.innerHTML = `
     <div class="group-by">
@@ -322,7 +287,6 @@ export async function updateStaffWiseSummary() {
   `;
 }
 
-
-export function printTransaction(){
+export function printTransaction() {
   window.print();
 }

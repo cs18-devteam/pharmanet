c88a9230611ed781909e207d61274604d7bb5493
ChamaniNiroshani <mpcniroshani@gmail.com>
2026-04-12 15:29:05 +0530

    document view

--- server/common/File.js
+++ server/common/File.js
@@ -1,45 +1,39 @@
-const fs = require('fs/promises');
-const path = require('path');
-const { updateFileServerCache } = require('../fileServer');
+const fs = require("fs/promises");
+const path = require("path");
+const { updateFileServerCache } = require("../fileServer");
 
+class File {
+  constructor({ fileName, content, mimeType }) {
+    this.fileName = fileName;
+    this.content = content;
+    this.mimeType = mimeType;
+  }
 
-class File{
-    constructor({fileName , content , mimeType}){
-        this.fileName = fileName;
-        this.content = content
-        this.mimeType = mimeType;
+  rename(newName) {
+    this.fileName = newName;
+  }
+
+  async save(filePath = "./") {
+    try {
+      filePath = path.join(__dirname, "/../storage/", filePath, this.fileName);
+      const file = await fs.writeFile(filePath, this.content, {
+        encoding: "binary",
+      });
+
+      updateFileServerCache();
+
+      return {
+        status: "success",
+        path: filePath,
+      };
+    } catch (e) {
+      console.log(e);
+      return {
+        status: "error",
+        error: e,
+      };
     }
-
-    rename(newName){
-        this.fileName = newName;
-    }
-
-
-    async save(filePath = "./"){
-        try{
-            filePath = path.join(__dirname , "/../storage/" ,filePath , this.fileName); 
-            const file = await fs.writeFile(filePath , this.content , {
-                encoding : "binary"
-            });
-
-            updateFileServerCache();
-            
-            return {
-                status:"success",
-                path: filePath ,
-            }
-            
-            
-
-        }catch(e){
-            console.log(e);
-            return {
-                status :'error',
-                error:e,
-            }
-            
-        }
-    }
+  }
 }
 
 module.exports = File;
